92f86b5adf5cc799ae6f39d518b2b42c0aa5ca23
Ladino Anselmo <ladans.me@gmail.com>
2025-09-04 01:50:27 +0200

    Initial Commit

---
+++ CustomAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomAlert
+//
+//  Created by ladans on 04/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by ladans on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
